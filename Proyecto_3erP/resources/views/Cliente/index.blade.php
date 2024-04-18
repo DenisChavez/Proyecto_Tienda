67cb84146a597cd2b41a98f6fe515512f5ab26ca
Karurosu <karurosu.vihir@gmail.com>
2024-04-17 22:30:33 -0600

    Resto de categorias

--- Proyecto_3erP/resources/views/Cliente/index.blade.php
+++ Proyecto_3erP/resources/views/Cliente/index.blade.php
@@ -36,38 +36,38 @@
 
 <div class="list">
     <ul>
-        <a href="{{ route('cliente.producto', ['nombre' => 'camisa']) }}">
+        <a href="{{ route('cliente.producto', ['nombre' => 'ropa interior']) }}">
             <li class="category">
-                <img src="img/camisas.jpg" alt="camisas" class="img-cat">
-                <p class="title-cat">Camisas</p>
-                <p class="p-cat">Prendas superiores de manga corta, larga o sin manga, pueden tener botones con cuello</p>
+                <img src="img/ropa_interior.jpg" alt="ropa interior" class="img-cat">
+                <p class="title-cat">Ropa Interior</p>
+                <p class="p-cat">Todo tipo de ropa interior para mujeres y para hombres</p>
                
             </li>
         </a>
-        <a href="{{ route('cliente.producto', ['nombre' => 'pantalon']) }}"> 
+        <a href="{{ route('cliente.producto', ['nombre' => 'sueteres']) }}"> 
             <li class="category" style="margin-left: -20%;">
-                <img src="img/pantalones.PNG" alt="pantalones" class="img-cat">
-                <p class="title-cat">Pantalones</p>
-                <p class="p-cat">Prendas inferiores que cubren las piernas, incluimos faladas y shorts</p>    
+                <img src="img/sueter.jpg" alt="sueteres" class="img-cat">
+                <p class="title-cat">Sueteres</p>
+                <p class="p-cat">Todo tipo de sueteres y chumpas de diferentes estilos</p>    
             </li>
         </a>
     </ul>  
 </div>
 <div class="list">
     <ul>
-        <a href="{{ route('cliente.producto', ['nombre' => 'camisa']) }}">
+        <a href="{{ route('cliente.producto', ['nombre' => 'formal']) }}">
             <li class="category">
-                <img src="img/camisas.jpg" alt="camisas" class="img-cat">
-                <p class="title-cat">Camisas</p>
-                <p class="p-cat">Prendas superiores de manga corta, larga o sin manga, pueden tener botones con cuello</p>
+                <img src="img/vestido.jpg" alt="Ropa formal" class="img-cat">
+                <p class="title-cat">Formal</p>
+                <p class="p-cat">Prendas formales incluidos los sacos y los vestidos</p>
                
             </li>
         </a>
-        <a href="{{ route('cliente.producto', ['nombre' => 'pantalon']) }}"> 
+        <a href="{{ route('cliente.producto', ['nombre' => 'otros']) }}"> 
             <li class="category" style="margin-left: -20%;">
-                <img src="img/pantalones.PNG" alt="pantalones" class="img-cat">
-                <p class="title-cat">Pantalones</p>
-                <p class="p-cat">Prendas inferiores que cubren las piernas, incluimos faladas y shorts</p>    
+                <img src="img/otro.jpg" alt="Otros tipos de ropa" class="img-cat">
+                <p class="title-cat">Otros</p>
+                <p class="p-cat">Otros tipos de ropa</p>    
             </li>
         </a>
     </ul>  
