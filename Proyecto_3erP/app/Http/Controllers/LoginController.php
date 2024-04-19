725149637d38636c1c5d67e00f799f38d61d534f
Karurosu <karurosu.vihir@gmail.com>
2024-04-18 19:25:35 -0600

    final2

--- Proyecto_3erP/app/Http/Controllers/LoginController.php
+++ Proyecto_3erP/app/Http/Controllers/LoginController.php
@@ -47,5 +47,6 @@ class LoginController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
+        return view('Login.login')
     }
 }
